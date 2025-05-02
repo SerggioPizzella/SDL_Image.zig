17195dbe49a290db737869ec12c06faf526c51a9
Serggio Pizzella <serggio.pizzella-ricci@capgemini.com>
2025-05-02 15:50:26 +0200

    remove zig suffix

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const lib = b.addStaticLibrary(.{
-        .name = "SDL_Image.zig",
+        .name = "SDL_Image",
         .target = target,
         .optimize = optimize,
         .link_libc = true,
